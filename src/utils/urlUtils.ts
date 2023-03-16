e290d3851d5c24774a2671f4378ee5fd17fd85b6
Marcio Caraballo <marcio.caraballososa@gmail.com>
2023-03-16 05:30:02 -0300

    encode fix

--- src/utils/urlUtils.ts
+++ src/utils/urlUtils.ts
@@ -1,12 +1,19 @@
 const updateURLQueryParams = (name: string, value: string) => {
     const queryParams = new URLSearchParams(window.location.search)
-    queryParams.set(name, value)
+    queryParams.set(name, encodeURI(value))
     window.history.replaceState(null, '', '?' + queryParams.toString())
 }
 
 const getURLQueryParams = (name: string) => {
     const queryParams = new URLSearchParams(window.location.search)
-    return queryParams.get(name)
+
+    const query = queryParams.get(name)
+
+    if (query !== null) {
+        return decodeURI(query)
+    }
+
+    return undefined
 }
 
 export { updateURLQueryParams, getURLQueryParams }
